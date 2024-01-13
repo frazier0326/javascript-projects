e32b2e4ac283e8eb74317ae8ec2b5f8d1d0134a3
frazier0326 <frazier0326@gmail.com>
2024-01-13 08:34:28 -0600

    exercises complete

--- errors-and-debugging/chapter-examples/Syntax-Highlighting.js
+++ errors-and-debugging/chapter-examples/Syntax-Highlighting.js
@@ -1,2 +1,12 @@
-let name = Julie;
-console.log("Hello, name);
+
+
+cityName = "Vienna";
+stateName = "Virginia";
+location = cityName + ", " +  stateName;
+
+console.log(location.length);
+
+let language = "JavaScript";
+let languageNew = language.slice(1,6)
+
+console.log(languageNew);
--- stringing-characters-together/code-snippets/bracket-notation.js
+++ stringing-characters-together/code-snippets/bracket-notation.js
@@ -2,3 +2,6 @@ let jsCreator = "Brendan Eich";
 
 console.log(jsCreator[-1]);
 console.log(jsCreator[42]);
+
+let phrase = "JavaScript rocks!";
+console.log(phrase[phrase.length - 8]);
