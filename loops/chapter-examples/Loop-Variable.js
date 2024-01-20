0d61e319dee09d541bddea33e797b6b120ad6cae
frazier0326 <frazier0326@gmail.com>
2024-01-20 03:20:38 -0600

    exercises complete

--- loops/chapter-examples/Loop-Variable.js
+++ loops/chapter-examples/Loop-Variable.js
@@ -1,5 +1,10 @@
 // Experiment with this loop by modifying each of the following: the variable initialization, the boolean condition, and the update expression.
 
-for (let i = 0; i < 51; i++) {
+/*for (let i = 5; i < 500; i = i +5) {
    console.log(i);
+ }*/
+ let phrase = "Chili Cook-off";
+console.log(phrase.length);
+ for (let i = 0; i < phrase.length - 1; i = i + 3) {
+    console.log(phrase[i]);
  }
--- loops/chapter-examples/for-Loop-Practice-With-Arrays.js
+++ loops/chapter-examples/for-Loop-Practice-With-Arrays.js
@@ -1,3 +1,24 @@
 // create an array variable containing the names
-
+let names = ["Patrick", "Dawn", "Patrease", "Joe"];
 // write a for loop that prints each name on a different line
+for (let i=0; i<names.length; i++) {
+    console.log(names[i]);
+}
+for (let i = 3; i < 10; i++) {
+    console.log(i);
+ }
+ let name = "LaunchCode";
+
+for (let i = 6; i < name.length; i++) {
+   console.log(name[i]);
+}
+/*for (let i = 0; i > -1; i++) {
+    console.log("LaunchCode");
+ }*/
+ for (let i = 0; i < 51; i = i + 2) {
+    console.log(i);
+ }
+ /*for (let i = 0; i < 51; i--) {
+    console.log(i);
+ }*/
+ 
