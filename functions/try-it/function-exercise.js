63882af6dca0b9ac3781f860295bd71c6eb59e6c
frazier0326 <frazier0326@gmail.com>
2024-01-25 06:39:47 -0600

    bonus mission complete

--- functions/try-it/function-exercise.js
+++ functions/try-it/function-exercise.js
@@ -1,75 +1,76 @@
-function makeLine(size) {
+
+function makeLine(size, character = "#") {
     let line = '';
     for (let i=0; i < size; i++) {
-        line +='#';
+        line += character;
     }
     return line;
 }
 
-console.log(makeLine(10));
+console.log(makeLine(10, "%"));
 
-function makeSquare(size) {
-    let line = makeLine(size);
+function makeSquare(size, character) {
+    let line = makeLine(size, character);
     let square = '';
     for (let i=0; i<size; i++){
         square += line + '\n'
     }
-    return square;
+    return square.slice(0, -1);
 }
 
-console.log(makeSquare(3));
+console.log(makeSquare(3, "@"));
 
-function makeRectangle(width,height) {
+function makeRectangle(width,height, character) {
     let rectangle = '';
     for (let i=0; i<height; i++) {
-        rectangle += makeLine(width) + '\n'
+        rectangle += makeLine(width, character) + '\n'
     }
-    return rectangle;
+    return rectangle.slice(0, -1);
 }
 
-console.log(makeRectangle(5, 3));
+console.log(makeRectangle(5, 3, "!"));
 
-function makeDownwardStairs(height) {
+function makeDownwardStairs(height, character) {
     let line = '';
     for (i=0; i<height; i++) {
-        line += makeLine(i+1) + '\n'
+        line += makeLine(i+1, character) + '\n'
     }
-    return line;
+    return line.slice(0, -1);
 }
 
-console.log(makeDownwardStairs(5));
+console.log(makeDownwardStairs(5, "&"));
 
-function makeSpaceLine(numSpaces, numChars) {
+function makeSpaceLine(numSpaces, numChars, character) {
     let spaces = "";
-    let line = makeLine(numChars);
+    let line = makeLine(numChars, character);
     for (let i=0; i<numSpaces; i++) {
       spaces += " ";
     }
     let newLine = spaces + line + spaces;
     return newLine;
   }
-  console.log(makeSpaceLine(3, 5));
+  console.log(makeSpaceLine(3, 5, "~"));
 
-function makeIsoscelesTriangle(height) {
+function makeIsoscelesTriangle(height, character) {
     let triangle = '';
     for (let i = 0; i < height; i++) {
-      triangle += (makeSpaceLine(height - i - 1, 2*i + 1) + '\n');
+      triangle += (makeSpaceLine(height - i - 1, 2*i + 1, character) + '\n');
     }
     return triangle;
   }
 
-console.log(makeIsoscelesTriangle(5));
+console.log(makeIsoscelesTriangle(5, "%"));
 
 function reversed(str) {
     return str.split('').reverse().join('');
  }
 
-function makeDiamond(height) {
+function makeDiamond(height, character) {
     let diamond = '';
     for (i=0; i<height; i++) {
-        diamond =(makeIsoscelesTriangle(height));
+        diamond =(makeIsoscelesTriangle(height, character));
     }
-    let upsideDownDiamond = diamond + reversed(diamond);
+    let upsideDownDiamond = diamond.slice(0, -1) + reversed(diamond);
     return upsideDownDiamond;
 }
-console.log(makeDiamond(5));
+console.log(makeDiamond(5, "*"));
