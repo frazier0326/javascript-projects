802d9f2cb0f2743f85c2293ca95907a901c74f99
frazier0326 <frazier0326@gmail.com>
2024-01-29 19:56:04 -0600

    studio complete

--- more-on-functions/studio/part-one-find-minimum-value.js
+++ more-on-functions/studio/part-one-find-minimum-value.js
@@ -7,4 +7,15 @@ let nums3 = [200, 5, 4, 10, 8, 5, -3.3, 4.4, 0];
 
 //Using one of the test arrays as the argument, call your function inside the console.log statement below.
 
-console.log(/* your code here */);
+function minimumValue(arrayNums) {
+    let minimum = arrayNums[0];
+    for (let i=0; i<=arrayNums.length; i++) {
+        if (arrayNums[i] < minimum) {
+            minimum = arrayNums[i];
+        }
+    }
+        return minimum;
+    }
+
+
+console.log(minimumValue(nums2));
