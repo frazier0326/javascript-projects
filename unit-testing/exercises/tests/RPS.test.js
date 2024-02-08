282e7a5a30f826798e6e0508b1b1c6d0fd2680b1
frazier0326 <frazier0326@gmail.com>
2024-02-08 13:02:17 -0600

    exercises complete

--- unit-testing/exercises/tests/RPS.test.js
+++ unit-testing/exercises/tests/RPS.test.js
@@ -1,8 +1,17 @@
-const whoWon = require('../RPS.js');
+const won = require('../RPS.js');
 
 describe("whoWon", function(){
     test("returns 'TIE!' when players choose the same option", function(){
-        let output = test.whoWon('rock', 'rock')
+        let output = won.whoWon('rock', 'rock')
         expect(output).toBe('TIE!');
-    })
+    });
+    test("returns 'Player 2 wins!' if P1 = rock & P2 = paper", function(){
+        let output = won.whoWon('rock','paper');
+        expect(output).toBe("Player 2 wins!");
+     });
+     
+     test("returns 'Player 2 wins!' if P1 = paper & P2 = scissors", function(){
+        let output = won.whoWon('paper','scissors');
+        expect(output).toBe("Player 2 wins!");
+     });
 })
