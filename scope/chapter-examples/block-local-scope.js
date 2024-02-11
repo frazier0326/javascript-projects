baa9a994202a330414bed21517ec0b643165b092
frazier0326 <frazier0326@gmail.com>
2024-02-11 06:22:39 -0600

    exercises complete

--- scope/chapter-examples/block-local-scope.js
+++ scope/chapter-examples/block-local-scope.js
@@ -1,6 +1,25 @@
+
+let i = 10;
+
 function myFunction() {
-    let i = 10;
     return 10 + i;
 }
 
 console.log(i);
+console.log(myFunction());
+
+let a = 0;
+
+function coolFunction() {
+    let b = 2;
+    return a + b;
+}
+
+function coolerFunction() {
+    let c = 5;
+    c += coolFunction();
+    return c;
+}
+
+console.log(coolFunction());
+console.log(coolerFunction());
--- scope/chapter-examples/variable-shadowing.js
+++ scope/chapter-examples/variable-shadowing.js
@@ -1,18 +1,29 @@
-const input = require('readline-sync');
+// const input = require('readline-sync');
 
-function hello(name) {
-    console.log('Hello,', name);
-    name = 'Ruth';
-    return doubleName(name);
+// function hello(name) {
+//     console.log('Hello,', name);
+//     name = 'Ruth';
+//     return doubleName(name);
+// }
+
+// function doubleName(name){
+//     console.log(name+name);
+//     return name+name;
+// }
+
+// let name = input.question("Please enter your name: ");
+
+// hello(name);
+// doubleName(name);
+// console.log(name);
+
+let a = 0;
+
+function myFunction() {
+    let a = 10;
+    return a;
 }
 
-function doubleName(name){
-    console.log(name+name);
-    return name+name;
-}
-
-let name = input.question("Please enter your name: ");
-
-hello(name);
-doubleName(name);
-console.log(name);
+console.log(a);
+console.log(myFunction());
+console.log(a);
