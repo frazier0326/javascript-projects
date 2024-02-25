fef87eddc8063e3a6c9cc6f70af690e8b2a8eaee
frazier0326 <frazier0326@gmail.com>
2024-02-25 00:41:23 -0600

    exercises completed

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -4,7 +4,24 @@ function init () {
     const paragraph = document.getElementById("statusReport");
 
     // Put your code for the exercises here.
-    
+    button.addEventListener('click', event => {
+        paragraph.innerHTML = 'Houston! We have liftoff!';
+     });
+     missionAbort.addEventListener("mouseover", function() {
+        missionAbort.style.backgroundColor = 'red';
+    });
+     missionAbort.addEventListener("mouseout", function() {
+        missionAbort.style.backgroundColor = '';
+     });
+     missionAbort.addEventListener("click", function() {
+        let confirmAbort = window.confirm("Are you sure you want to abort the mission?");
+     if(confirmAbort) {
+         paragraph.innerHTML = "Mission aborted! Space shuttle returning home!";
+     }else{
+        paragraph.innerHTML = "Mission continues!";
+     }
+    });
+     
 }
 
 window.addEventListener("load", init);
